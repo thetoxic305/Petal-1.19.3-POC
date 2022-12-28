1fae7595e8f760545143c690c38149f2ecb8211d
5jd <85830207+5jd@users.noreply.github.com>
2022-12-28 01:14:17 +0100

    Updated Upstream (Purpur)
    Upstream has released updates that appear to apply and compile correctly
    
    Purpur Changes:
    PurpurMC/Purpur@b7ee6de Option to allow beacon effects when covered by tinted glass (#1233)
    PurpurMC/Purpur@fe3250c Updated Upstream (Paper)

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 repositories {
     mavenCentral()
-    maven("https://papermc.io/repo/repository/maven-public/") {
+    maven("https://repo.papermc.io/repository/maven-public/") {
         content { onlyForConfigurations(PAPERCLIP_CONFIG) }
     }
 }
@@ -38,7 +38,7 @@ subprojects {
         mavenLocal()
         mavenCentral()
         maven("https://oss.sonatype.org/content/groups/public/")
-        maven("https://papermc.io/repo/repository/maven-public/")
+        maven("https://repo.papermc.io/repository/maven-public/")
         maven("https://ci.emc.gs/nexus/content/groups/aikar/")
         maven("https://repo.aikar.co/content/groups/aikar")
         maven("https://repo.md-5.net/content/repositories/releases/")
@@ -67,5 +67,17 @@ paperweight {
             apiOutputDir.set(layout.projectDirectory.dir("petal-api"))
             serverOutputDir.set(layout.projectDirectory.dir("petal-server"))
         }
+    }
+}
+
+tasks.register("printMinecraftVersion") {
+    doLast {
+        println(providers.gradleProperty("mcVersion").get().trim())
+    }
+}
+
+tasks.register("printPaperVersion") {
+    doLast {
+        println(project.version)
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,30 @@
+if (!file(".git").exists()) {
+    val errorText = """
+        
+        =====================[ ERROR ]=====================
+         The Petal project directory is not a properly cloned Git repository.
+         
+         In order to build Purpur from source you must clone
+         the Purpur repository using Git, not download a code
+         zip from GitHub.
+         
+         Built Petal jars are available for download at
+         https://github.com/Bloom-host/Petal/releases/
+         
+         See Purpur's contributing guide https://github.com/PurpurMC/Purpur/blob/HEAD/CONTRIBUTING.md
+         for further information on building and modifying Petal.
+        ===================================================
+    """.trimIndent()
+    error(errorText)
+}
+
 pluginManagement {
     repositories {
         gradlePluginPortal()
-        maven("https://papermc.io/repo/repository/maven-public/")
+        maven("https://repo.papermc.io/repository/maven-public/")
     }
 }
 
 rootProject.name = "petal"
+
 include("petal-api", "petal-server")
